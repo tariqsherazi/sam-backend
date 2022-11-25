1b6451a51c0dccf5d19344e40992b4292e7ac648
Tariq Sherazi <zetariqkhan@gmail.com>
2022-11-25 08:23:16 -0800

    committed

--- Database/Db.js
+++ Database/Db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 const db=async()=>{
     try{
-        const conn=await mongoose.connect("mongodb+srv://tariqsherazi:tariqsherazi@cluster0.e0dz7.mongodb.net/demo?retryWrites=true&w=majority")
+        const conn=await mongoose.connect(`mongodb+srv://user-123:tariq123@mini-mern-tut.fvmq7.mongodb.net/demo?retryWrites=true&w=majority`)
         console.log("mongodb connect")
 
     }catch(err){
